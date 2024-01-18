3409c5920b8a1a3eedbcb8312bbd0264c347e5ce
Neirel Zapatos <neirelzapatos@gmail.com>
2024-01-17 20:27:41 -0800

    Added Project Added Functionality using SQL

---
+++ forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired, URL
+from wtforms import PasswordField, SubmitField, StringField
+
+class AddForm(FlaskForm):
+    password = PasswordField(label="Admin Password", validators=[DataRequired()])
+    submit = SubmitField(label="Submit")
+
+
+class AddProjectForm(FlaskForm):
+    project_name = StringField(label="Project Name", validators=[DataRequired()])
+    project_description = StringField(label="Project Description", validators=[DataRequired()])
+    project_url = StringField(label="Project URL")
+    project_github = StringField(label="Project Github URL")
+    submit = SubmitField(label="Submit")
--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String
+from forms import AddForm, AddProjectForm
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "R4jBwpb@V!w^7d3"
 Bootstrap5(app)
+
+admin_password = "1234"
 
 
 class Base(DeclarativeBase):
@@ -32,6 +35,12 @@ with app.app_context():
 
 @app.route("/")
 def home():
+    # new_project = Project(
+    #     name="Testing",
+    #     description="We don't really know",
+    # )
+    # db.session.add(new_project)
+    # db.session.commit()
     project_list = Project.query.all()
     return render_template("index.html", project_list=project_list)
 
@@ -40,6 +49,28 @@ def home():
 def projects():
     return render_template("projects.html")
 
+
+@app.route("/add-project", methods=["GET", "POST"])
+def add_project():
+    add_form = AddForm()
+    if add_form.validate_on_submit():
+        if add_form.password.data == admin_password:
+            return redirect(url_for("project_details"))
+    return render_template("add.html", form=add_form)
+
+
+@app.route("/project_details", methods=["GET", "POST"])
+def project_details():
+    project_form = AddProjectForm()
+    if project_form.validate_on_submit():
+        new_project = Project(
+            name=project_form.project_name.data,
+            description=project_form.project_description.data
+        )
+        db.session.add(new_project)
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("add_project.html", form=project_form)
 
 if __name__ == "__main__":
     app.run(debug=True)
